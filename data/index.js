be505f963a27ae56d8b0e9038b6b92c7f542a2de
Youngboy <nicksonmahachi339@gmail.com>
2025-09-03 20:40:42 +0200

    Add files via upload

---
+++ data/index.js
@@ -0,0 +1,38 @@
+const { AntiDelDB,
+    initializeAntiDeleteSettings,
+    setAnti,
+    getAnti,
+    getAllAntiDeleteSettings, } = require('./antidel');
+const {
+    saveContact,
+    loadMessage,
+    getName,
+    getChatSummary,
+    saveGroupMetadata,
+    getGroupMetadata,
+    saveMessageCount,
+    getInactiveGroupMembers,
+    getGroupMembersMessageCount,
+    saveMessage,
+} = require('./store');
+
+
+module.exports = {
+    AntiDelDB,
+    initializeAntiDeleteSettings,
+    setAnti,
+    getAnti,
+    getAllAntiDeleteSettings,
+    saveContact,
+    loadMessage,
+    getName,
+    getChatSummary,
+    saveGroupMetadata,
+    getGroupMetadata,
+    saveMessageCount,
+    getInactiveGroupMembers,
+    getGroupMembersMessageCount,
+    saveMessage,
+};
+
+// code by 𝙼𝙸𝙳𝙺𝙸𝙽𝙶𝙾𝙸-𝚃𝙴𝙲𝙷 🕸️👻
